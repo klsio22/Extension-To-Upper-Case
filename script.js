fdc2beea47b8a639425c13830c97b3a1d845f057
klesio25 <klesio.k9@gmail.com>
2021-09-19 11:24:47 -0300

    Create functions to conversion words

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-documento.addEventListener(
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#btn").addEventListener("click", function () {
+    let text = document.querySelector("#text").value;
+    document.querySelector("#result").innerHTML = word(text);
+  });
 
-  
-)
+  let word = (text) => {
+    let words = text.split(" ");
+    let lowerdText = text.toLowerCase();
+
+    for (let i = 0; i < words.length; i++) {
+      let newWord = words[i];
+
+      let fistLetter = newWord[0];
+      newWord = fistLetter.toUpperCase() + newWord.slice(1);
+
+      words[i] = newWord;
+    }
+
+    return words.join("");
+  };
+
+});
